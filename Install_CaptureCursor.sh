ec6f0816e36d98c286a600ef6b18b8f677372b24
skipcress <77211825+skipcress@users.noreply.github.com>
2023-06-29 17:00:35 -0700

    Update Install_CaptureCursor.sh

--- Install_CaptureCursor.sh
+++ Install_CaptureCursor.sh
@@ -21,22 +21,27 @@ function escalate_privilages() {
 	sudo echo > /dev/null
 }
 
+# Function returns directory name for temp directory
+function get_temp_dir_name() {
+	dirName=$(uuidgen)
+ 	echo "${HOME}/Documents/.${dirName//-/}"
+}
+
+# Function deletes the temp directory, if it exists
+function cleanup() {
+	if [ -d "$tempDir" ]; then
+ 		echo "\n\033[32mDeleting temp folder...\033[0m\n"
+ 		rm -rf "$tempDir"
+   	fi
+}
+
+# Trap on exit, run cleanup funtion
+trap cleanup EXIT
+
 # 1. Install dependencies
 echo "\n\033[32m#########################################################################\033[0m"
 echo "\033[32m##### Installing software dependencies (this may take some time)... #####\033[0m"
 echo "\033[32m#########################################################################\033[0m\n"
-
-## Install XCode
-#echo "\n\033[32mChecking if XCode Command Line Developer Tools are installed...\033[0m"
-#xcode-select -p > /dev/null
-#if [[ $? -gt 0 ]]; then
-#	echo "\n\033[32mXCode not installed; installing XCode...\033[0m\n"
-#	echo "\033[32mPress the 'Install' button, then agree to the ULA to complete this installation.\033[0m\n"
-# 	xcode-select --install
-#	exit_on_fail
-#else
-#	echo "\n\033[32mXCode already installed.\033[0m\n"
-#fi
 
 echo "\n\033[32mChecking if Homebrew is installed...\033[0m\n"
 which -s brew > /dev/null
@@ -54,7 +59,7 @@ fi
 
 # Install git
 echo "\n\033[32mInstalling git (if not already installed)\nFollow prompts to install software.\033[0m\n"
-git --version
+brew install git
 exit_on_fail
 
 # Install CLIClick
@@ -68,11 +73,12 @@ echo "\033[32m##### Copying files from GitHub... #####\033[0m"
 echo "\033[32m########################################\033[0m\n"
 
 echo "\n\033[32mCreating temp directory...\033[0m"
-mkdir ~/Documents/CaptureCursor
+tempDir=$(get_temp_dir_name)
+mkdir "$tempDir"
 exit_on_fail
 echo "\033[32mMoving to temp directory...\033[0m"
 exit_on_fail
-cd ~/Documents/CaptureCursor
+cd "$tempDir"
 echo "\033[32mUsing git to fetch necessary files...\033[0m"
 git clone $gitRepoPath
 exit_on_fail
@@ -82,7 +88,7 @@ echo "\n\033[32m########################################\033[0m"
 echo "\033[32m##### Compiling the Application... #####\033[0m"
 echo "\033[32m########################################\033[0m\n"
 
-osacompile -o ~/Documents/CaptureCursor/CaptureCursor.app ~/Documents/CaptureCursor/CaptureCursor/MacOS/CaptureCursor.applescript
+osacompile -o "${tempDir}/CaptureCursor.app" "{$tempDir}/CaptureCursor/MacOS/CaptureCursor.applescript"
 exit_on_fail
 
 # 4. Install application
@@ -92,16 +98,16 @@ echo "\033[32m######################################\033[0m\n"
 
 escalate_privilages
 echo "\n\033[32mCopying base script...\033[0m\n"
-sudo cp ~/Documents/CaptureCursor/CaptureCursor/MacOS/CaptureCursor.sh /usr/local/bin
+sudo cp "${tempDir}/CaptureCursor/MacOS/CaptureCursor.sh" "/usr/local/bin/"
 exit_on_fail
 echo "\n\033[32mMaking base script executable...\033[0m\n"
 sudo chmod +x /usr/local/bin/CaptureCursor.sh
 exit_on_fail
 echo "\n\033[32mCopying compiled application...\033[0m\n"
-sudo cp -R ~/Documents/CaptureCursor/CaptureCursor.app /Applications/
+sudo cp -R "${tempDir}/CaptureCursor.app" "/Applications/"
 exit_on_fail
 echo "\n\033[32mCopying new icon to Resources directory...\033[0m\n"
-sudo cp ~/Documents/CaptureCursor/CaptureCursor/MacOS/CaptureCursor.icns /Applications/CaptureCursor.app/Contents/Resources/applet.icns
+sudo cp "${tempDir}/CaptureCursor/MacOS/CaptureCursor.icns" "/Applications/CaptureCursor.app/Contents/Resources/applet.icns"
 exit_on_fail
 
 # 5. Cleaning up temporary files
@@ -109,8 +115,7 @@ echo "\n\033[32m#######################################\033[0m"
 echo "\033[32m##### Cleaning up Temporary Files #####\033[0m"
 echo "\033[32m#######################################\033[0m\n"
 
-echo "\n\033[32mDeleting temp folder (any failures at this stage will not effect the running of the application)...\033[0m\n"
-rm -rf ~/Documents/CaptureCursor
+cleanup
 
 # All operations complete
 echo "\n\033[32m####################################\033[0m"
