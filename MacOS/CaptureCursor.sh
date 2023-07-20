278ac53692fabd8f516be2beb5b018698afa05e4
skipcress <77211825+skipcress@users.noreply.github.com>
2023-07-20 11:56:37 -0700

    Update CaptureCursor.sh
    Changed behavior of script to move cursor to closest screen edge, rather than the center of the screen. Increased cursor monitoring to every 0.25 seconds from 3 seconds.

--- MacOS/CaptureCursor.sh
+++ MacOS/CaptureCursor.sh
@@ -1,26 +1,24 @@
-#!/bin/sh
-
-# Checks if process is already running
-processCount=$(ps -ef | grep 'CaptureCursor.sh' | wc -l)
-if [ $((processCount)) -gt 3 ]; then
-	exit 0;
-fi
+#!/bin/bash
 
 # Checks if cursor is outide bounds of primary monitor
-# if so, moves the cursor to center point of monitor
+# if so, moves the cursor to the nearest monitor edge
 function move_cursor() {
-	echo "Checking if cursor is in primary screen bounds..."
-	if [ "$CX" -gt "$X" ] || [ "$CY" -gt "$Y" ] || [ "$CX" -lt 0 ] || [ "$CY" -lt 0 ]; then
-		cliclick m:$((X / 2)),$((Y / 2)) p
-		echo "Cursor moved to: x=$((X / 2)), y=$((Y / 2))"
-	else
-		echo "Cursor is within bounds, no action taken."
+	if [ "$CX" -gt "$X" ]; then
+		echo "Cursor moved (PRESS CTRL+C TO EXIT)"
+		cliclick m:$X,$CY p
 	fi
-}
-
-# Pads input with leading zeros, up to 5 digits
-function pad() {
-	printf "%05d" $1
+	if [ "$CX" -lt 0 ]; then
+		echo "Cursor moved (PRESS CTRL+C TO EXIT)"
+		cliclick m:0,$CY p
+	fi
+	if [ "$CY" -gt "$Y" ]; then
+		echo "Cursor moved (PRESS CTRL+C TO EXIT)"
+		cliclick m:$CX,$Y p
+	fi
+	if [ "$CY" -lt 0 ]; then
+		echo "Cursor moved (PRESS CTRL+C TO EXIT)"
+		cliclick m:$CX,0 p
+	fi
 }
 
 # Clears the screen on exit
@@ -33,10 +31,10 @@ function cleanup() {
 trap cleanup SIGINT 
 
 # Gets width of primary monitor
- X=$(system_profiler SPDisplaysDataType | grep Resolution | awk 'NR==1{print $2;}')
+X=$(system_profiler SPDisplaysDataType | grep Resolution | awk 'NR==1{print $2;}')
 
 # Gets height of primary monitor
- Y=$(system_profiler SPDisplaysDataType | grep Resolution | awk 'NR==1{print $4;}')
+Y=$(system_profiler SPDisplaysDataType | grep Resolution | awk 'NR==1{print $4;}')
 
 # Clears screen, and prints monitor resolution to console
 clear
@@ -44,10 +42,9 @@ echo "Resolution detected for primary monitor: ${X}x${Y}"
 echo ""
 
 # Sets variable that specifies frequency of cursor location check
-sleepSecs=3
+sleepSecs=0.25
 
-# Counter keeps track of iterations
-counter=0
+echo "Beginning cursor position monitoring; PRESS CTRL+C TO EXIT"
 
 # Infinite loop gets current cursor location, and passes it to move_cursor()
 while true 
@@ -56,8 +53,5 @@ do
 	CY=$(cliclick p | awk 'BEGIN { FS = "," }; {print $2}')
 	move_cursor $CX $CY $X $Y
 	
-	echo ""
-	((counter++))
-	echo "$(pad $counter): Sleeping for $sleepSecs second(s) - \033[32mPress CTRL+C to exit\033[0m"
 	sleep $sleepSecs
 done
